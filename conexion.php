7bfccb978a072d360268ee1a8febdb0ad9578323
Tulsi Guanipa <tulsiguanipa@gmail.com>
2019-12-14 00:01:32 -0300

    modificacion bd

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php 
- $conexion =mysqli_connect('localhost','root','', 'tulsiguanipaconsultas') or die (mysqli_connect_error());
+ $conexion =mysqli_connect('localhost','id10515664_tulsiguanipa','guanipa1995', 'id10515664_tulsiguanipa') or die (mysqli_connect_error());
 
 
 ?>
